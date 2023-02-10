e498fb481b9bd679ac8338b282159bac1b098574
Ekaterina <katushka26@yandex.ru>
2023-02-10 13:12:46 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите число: ");
+int x = int.Parse (Console.ReadLine()!);
+
+if (x % 2 == 0)
+{
+    Console.WriteLine ("Число " + x + " - четное ");
+}
+else
+{
+        Console.WriteLine ("Число " + x + " - нечетное ");
+}
