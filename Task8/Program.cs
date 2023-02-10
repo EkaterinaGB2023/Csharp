02753cae88e0b82301f7004db15632d8d01613f1
Ekaterina <katushka26@yandex.ru>
2023-02-10 14:36:33 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите целое число: ");
+int number = int.Parse (Console.ReadLine()!); //преобразовываем строку и считываем
+int even_num = 2;
+Console.Write($"{number} -> ");
+
+while (even_num < number - 1)
+{
+Console.Write($"{even_num}, ");
+even_num += 2;
+}
+Console.Write($"{even_num}");
