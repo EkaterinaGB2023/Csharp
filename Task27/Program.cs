ebe6243d8d43c79e31daf71345e603861d4ee6f3
Ekaterina <katushka26@yandex.ru>
2023-02-21 15:59:26 +0300

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int Prompt (string message)
+{
+    Console.WriteLine (message);
+    int arg = int.Parse (Console.ReadLine()!); 
+    return arg;
+}
+
+int CountSumOfDigits (int num)
+{
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + (num % 10);
+    num /= 10; 
+}
+return sum;
+}
+
+int number = Prompt ("Введите число: ");
+int result = CountSumOfDigits (number);
+Console.WriteLine ($"Сумма цифр числа {number} равна {result}");
